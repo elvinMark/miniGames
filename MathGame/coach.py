80a19153dd78a36982e4dbb6b61a3891686c9086
Elvin Mark <elvin.mark.titech@gmail.com>
2021-09-02 17:37:36 +0900

    fixing code

--- MathGame/coach.py
+++ MathGame/coach.py
@@ -1,5 +1,13 @@
 import random
 import time
+import argparse
+
+parser = argparse.ArgumentParser("This is a simple math game!")
+
+parser.add_argument("--levels",type=int,default=1,help="specify the number of levels you want to play")
+parser.add_argument("--questions",type=int,default=10,help="specify the number of questions in each level")
+
+args = parser.parse_args()
 
 def measureTime(g):
     def wrapper(*args):
@@ -33,20 +41,30 @@ def createQuestion(digits,numTerms,op):
     return eval(s),s
 
 @measureTime
-def runGame(num_questions,num_levels):
+def runLevel(num_questions,level):
     score = 0
-    for level in range(1,num_levels):
-        digits,num_terms,op = getLevelInfo(level)
-        for q in range(num_questions):
-            ans,question = createQuestion(digits,num_terms,op)
-            print(question,'?')
-            user = int(input("Your answer:"))
-            if user == ans:
-                print(chr(9989))
-                score += 1
-            else:
-                print(chr(10060))
+    digits,num_terms,op = getLevelInfo(level)
+    for q in range(num_questions):
+        ans,question = createQuestion(digits,num_terms,op)
+        print(question,'?')
+        user = int(input("Your answer:"))
+        if user == ans:
+            print(chr(9989))
+            score += 1
+        else:
+            print(chr(10060))
     return score
 
-time,score = runGame(10,2)
-print("Your time was :%d \nAnd your score was: %d"%(int(time),score))
+def wait(s):
+    for i in range(s):
+        print(f"{i}...")
+        time.sleep(1)
+
+def runGame(num_questions,num_levels):
+    for level in range(1,num_levels + 1):
+        input(f"Level: {level}")
+        wait(3)
+        time, score = runLevel(num_questions,level)
+        print("Your time was :%d min. %d sec. \nAnd your score was: %d"%(time//60,time%60,score))
+
+runGame(args.questions,args.levels)
